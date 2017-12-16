e07852ca578f74345817c68b70c5e09465bd6ad5
Kiwilicious <jhwang137@gmail.com>
2017-12-16 02:27:02 -0800

    Add example links to several tech icons

--- src/pages/index.js
+++ src/pages/index.js
@@ -15,29 +15,64 @@ const IndexPage = () => (
     <h4 className="tech-icon-header">Frontend</h4>
     <hr />
     <div className="tech-icon">
-      <i className="devicon-javascript-plain colored" />
-      <i className="devicon-html5-plain-wordmark colored" />
-      <i className="devicon-css3-plain-wordmark colored" />
-      <i className="devicon-jquery-plain-wordmark colored" />
-      <i className="devicon-bootstrap-plain-wordmark colored" />
-      <i className="devicon-react-original-wordmark colored" />
-      <i className="devicon-angularjs-plain colored" />
-      <i className="devicon-d3js-plain colored" />
+      <div>
+        <i className="devicon-javascript-plain colored" />
+      </div>
+      <div>
+        <i className="devicon-html5-plain-wordmark colored" />
+      </div>
+      <div>
+        <i className="devicon-css3-plain-wordmark colored" />
+      </div>
+      <div>
+        <i className="devicon-jquery-plain-wordmark colored" />
+        <a href="https://github.com/Kiwilicious/tinychat/blob/master/js/app-main.js" target="_blank">Example</a>
+      </div>
+      <div>
+        <i className="devicon-bootstrap-plain-wordmark colored" />
+      </div>
+      <div>
+        <i className="devicon-react-original-wordmark colored" />
+        <a href="https://github.com/Kiwilicious/CARe/blob/master/client/app/containers/navBar/NavigationBar.jsx" target="_blank">Example</a>
+      </div>
+      <div>
+        <i className="devicon-angularjs-plain colored" />
+      </div>
+      <div>
+        <i className="devicon-d3js-plain colored" />
+      </div>
     </div>
     <h4 className="tech-icon-header">Backend</h4>
     <hr />
     <div className="tech-icon">
-      <i className="devicon-nodejs-plain colored" />
-      <i className="devicon-express-original colored" />
-      <i className="devicon-mysql-plain-wordmark colored" />
-      <i className="devicon-postgresql-plain-wordmark colored" />
-      <i className="devicon-sequelize-plain colored" />
+      <div>
+        <i className="devicon-nodejs-plain colored" />
+      </div>
+      <div>
+        <i className="devicon-express-original colored" />
+        <a href="https://github.com/Kiwilicious/CARe/blob/master/server/index.js" target="_blank">Example</a>
+      </div>
+      <div>
+        <i className="devicon-mysql-plain-wordmark colored" />
+      </div>
+      <div>
+        <i className="devicon-postgresql-plain-wordmark colored" />
+        <a href="https://github.com/Kiwilicious/CARe/blob/master/server/controllers/searchCtrl.js" target="_blank">Example</a>
+      </div>
+      <div>
+        <i className="devicon-sequelize-plain colored" />
+        <a href="https://github.com/Kiwilicious/CARe/blob/master/db/index.js" target="_blank">Example</a>
+      </div>
     </div>
     <h4 className="tech-icon-header">Other</h4>
     <hr />
     <div className="tech-icon">
-      <i className="devicon-git-plain colored" />
-      <i className="devicon-heroku-line-wordmark colored" />
+      <div>
+        <i className="devicon-git-plain colored" />
+      </div>
+      <div>
+        <i className="devicon-heroku-line-wordmark colored" />
+      </div>
     </div>
     {/* <Link to="/page-2/">Go to page 2</Link> */}
   </div>
