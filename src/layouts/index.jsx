cdfddae1ea8873f1e063b68092cd641f42025a5f
James <jhwang137@gmail.com>
2018-02-12 15:28:01 -0800

    Remade navbar with material-ui and cleaned up entry point

--- src/layouts/index.jsx
+++ src/layouts/index.jsx
@@ -1,98 +1,41 @@
+import 'typeface-roboto';
 import React from 'react';
 import PropTypes from 'prop-types';
-import Link from 'gatsby-link';
 import Helmet from 'react-helmet';
+import { withStyles } from 'material-ui/styles';
+import Reboot from 'material-ui/Reboot';
+import NavBar from './navbar';
 
-import './index.css';
+const styles = () => ({
+  root: {
+    fontFamily: 'roboto',
+    margin: '0 auto',
+    maxWidth: 960,
+    padding: '0px 1rem 1.45rem',
+  },
+});
 
-const Header = () => (
-  <div
-    style={{
-      background: '#8BB7E3',
-      marginBottom: '1.45rem',
-    }}
-  >
-    <div
-      style={{
-        'align-items': 'baseline',
-        display: 'flex',
-        'flex-direction': 'row',
-        margin: '0 auto',
-        maxWidth: 960,
-        padding: '1.45rem 1.0875rem',
-      }}
-    >
-      <h1 style={{ margin: 0 }}>
-        <Link
-          to="/"
-          style={{
-            color: 'white',
-            textDecoration: 'none',
-          }}
-        >
-          Home
-        </Link>
-      </h1>
-      <h3 style={{ margin: 0 }}>
-        <Link
-          to="/coding-challenges"
-          style={{
-            color: 'white',
-            'margin-left': '1em',
-            textDecoration: 'none',
-          }}
-        >
-          Coding Challenges
-        </Link>
-      </h3>
+const TemplateWrapper = (props) => {
+  const { classes, children } = props;
+  return (
+    <div>
+      <Helmet>
+        <title>James Hwang&apos;s Portfolio Site</title>
+        <meta name="Portfolio Site" content="Aggregation of James&apos; works" />
+      </Helmet>
+
+      <Reboot />
+      <NavBar />
+      <div className={classes.root}>
+        {children()}
+      </div>
     </div>
-  </div>
-);
-
-const TemplateWrapper = ({ children }) => (
-  <div>
-    <Helmet>
-      <title>James Hwang&apos;s Portfolio Site</title>
-      <meta name="Portfolio Site" content="Aggregation of James&apos; works" />
-
-      <link href="https://fonts.googleapis.com/css?family=Lato:300,300i,400,400i,700" rel="stylesheet" />
-
-      <link rel="stylesheet" href="https://cdn.rawgit.com/konpa/devicon/df6431e323547add1b4cf45992913f15286456d3/devicon.min.css" />
-      <link rel="stylesheet" href="https://cdn.rawgit.com/konpa/devicon/df6431e323547add1b4cf45992913f15286456d3/devicon.min.css" />
-      <link rel="stylesheet" href="https://cdn.rawgit.com/konpa/devicon/df6431e323547add1b4cf45992913f15286456d3/devicon.min.css" />
-      <link rel="stylesheet" href="https://cdn.rawgit.com/konpa/devicon/df6431e323547add1b4cf45992913f15286456d3/devicon.min.css" />
-      <link rel="stylesheet" href="https://cdn.rawgit.com/konpa/devicon/df6431e323547add1b4cf45992913f15286456d3/devicon.min.css" />
-      <link rel="stylesheet" href="https://cdn.rawgit.com/konpa/devicon/df6431e323547add1b4cf45992913f15286456d3/devicon.min.css" />
-      <link rel="stylesheet" href="https://cdn.rawgit.com/konpa/devicon/df6431e323547add1b4cf45992913f15286456d3/devicon.min.css" />
-      <link rel="stylesheet" href="https://cdn.rawgit.com/konpa/devicon/df6431e323547add1b4cf45992913f15286456d3/devicon.min.css" />
-      <link rel="stylesheet" href="https://cdn.rawgit.com/konpa/devicon/df6431e323547add1b4cf45992913f15286456d3/devicon.min.css" />
-      <link rel="stylesheet" href="https://cdn.rawgit.com/konpa/devicon/df6431e323547add1b4cf45992913f15286456d3/devicon.min.css" />
-      <link rel="stylesheet" href="https://cdn.rawgit.com/konpa/devicon/df6431e323547add1b4cf45992913f15286456d3/devicon.min.css" />
-      <link rel="stylesheet" href="https://cdn.rawgit.com/konpa/devicon/df6431e323547add1b4cf45992913f15286456d3/devicon.min.css" />
-      <link rel="stylesheet" href="https://cdn.rawgit.com/konpa/devicon/df6431e323547add1b4cf45992913f15286456d3/devicon.min.css" />
-      <link rel="stylesheet" href="https://cdn.rawgit.com/konpa/devicon/df6431e323547add1b4cf45992913f15286456d3/devicon.min.css" />
-      <link rel="stylesheet" href="https://cdn.rawgit.com/konpa/devicon/df6431e323547add1b4cf45992913f15286456d3/devicon.min.css" />
-    </Helmet>
-    <Header />
-    <div
-      style={{
-        margin: '0 auto',
-        maxWidth: 960,
-        padding: '0px 1.0875rem 1.45rem',
-        paddingTop: 0,
-      }}
-    >
-      {children()}
-    </div>
-  </div>
-);
+  );
+};
 
 TemplateWrapper.propTypes = {
-  children: PropTypes.func,
+  children: PropTypes.func.isRequired,
+  classes: PropTypes.func.isRequired,
 };
 
-TemplateWrapper.defaultProps = {
-  children: () => {},
-};
-
-export default TemplateWrapper;
+export default withStyles(styles)(TemplateWrapper);
---
+++ src/layouts/navbar.jsx
@@ -0,0 +1,63 @@
+import React from 'react';
+import PropTypes from 'prop-types';
+import Link from 'gatsby-link';
+import { withStyles } from 'material-ui/styles';
+import AppBar from 'material-ui/AppBar';
+import Toolbar from 'material-ui/Toolbar';
+import Grid from 'material-ui/Grid';
+import Typography from 'material-ui/Typography';
+import HomeIcon from 'material-ui-icons/Home';
+
+const styles = theme => ({
+  root: {
+    width: '100%',
+  },
+  link: {
+    color: 'white',
+    textAlign: 'center',
+    textDecoration: 'none',
+    '&:hover': {
+      color: theme.palette.text.secondary,
+    },
+  },
+  icon: {
+    margin: theme.spacing.unit,
+  },
+});
+
+const NavBar = (props) => {
+  const { classes } = props;
+  return (
+    <div className={classes.root}>
+      <AppBar position="static">
+        <Toolbar>
+          <Grid container spacing={16} justify="center">
+            <Grid item sm={2}>
+              <Link to="/coding-challenges" className={classes.link}>
+                <Typography variant="title" color="inherit">
+                  Code Samples
+                </Typography>
+              </Link>
+            </Grid>
+            <Link to="/" className={classes.link}>
+              <HomeIcon className={classes.icon} />
+            </Link>
+            <Grid item sm={2}>
+              <Link to="/contact" className={classes.link}>
+                <Typography variant="title" color="inherit">
+                  Contact
+                </Typography>
+              </Link>
+            </Grid>
+          </Grid>
+        </Toolbar>
+      </AppBar>
+    </div>
+  );
+};
+
+NavBar.propTypes = {
+  classes: PropTypes.func.isRequired,
+};
+
+export default withStyles(styles)(NavBar);
