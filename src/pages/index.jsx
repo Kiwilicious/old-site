51ab8081beb3e6cb8ea67059b0aa7bbcfbba7bed
James <jhwang137@gmail.com>
2018-02-12 15:54:57 -0800

    Home uses material-ui and moved coding examples to relevant page

--- src/pages/index.jsx
+++ src/pages/index.jsx
@@ -1,81 +1,33 @@
 import React from 'react';
+import PropTypes from 'prop-types';
+import { withStyles } from 'material-ui/styles';
+import Grid from 'material-ui/Grid';
+import Typography from 'material-ui/Typography';
 
-const IndexPage = () => (
-  <div>
-    <p
-      style={{
-        fontSize: '20px',
-        fontWeight: 300,
-      }}
-    >
-      I am a Software engineer specializing in frontend web development with a passion
-      of making elegant and lightweight single page applications. Also a game
-      enthusiast with an interest in the rising prominence of esports. Below are
-      some of the technologies I work with.
-    </p>
-    <h4 className="tech-icon-header">Frontend</h4>
-    <hr />
-    <div className="tech-icon">
-      <div>
-        <i className="devicon-javascript-plain colored" />
-      </div>
-      <div>
-        <i className="devicon-html5-plain-wordmark colored" />
-      </div>
-      <div>
-        <i className="devicon-css3-plain-wordmark colored" />
-      </div>
-      <div>
-        <i className="devicon-jquery-plain-wordmark colored" />
-        <a href="https://github.com/Kiwilicious/tinychat/blob/master/js/app-main.js" target="_blank" rel="noopener noreferrer">Example</a>
-      </div>
-      <div>
-        <i className="devicon-bootstrap-plain-wordmark colored" />
-      </div>
-      <div>
-        <i className="devicon-react-original-wordmark colored" />
-        <a href="https://github.com/Kiwilicious/CARe/blob/master/client/app/containers/navBar/NavigationBar.jsx" target="_blank" rel="noopener noreferrer">Example</a>
-      </div>
-      <div>
-        <i className="devicon-angularjs-plain colored" />
-      </div>
-      <div>
-        <i className="devicon-d3js-plain colored" />
-      </div>
-    </div>
-    <h4 className="tech-icon-header">Backend</h4>
-    <hr />
-    <div className="tech-icon">
-      <div>
-        <i className="devicon-nodejs-plain colored" />
-      </div>
-      <div>
-        <i className="devicon-express-original colored" />
-        <a href="https://github.com/Kiwilicious/CARe/blob/master/server/index.js" target="_blank" rel="noopener noreferrer">Example</a>
-      </div>
-      <div>
-        <i className="devicon-mysql-plain-wordmark colored" />
-      </div>
-      <div>
-        <i className="devicon-postgresql-plain-wordmark colored" />
-        <a href="https://github.com/Kiwilicious/CARe/blob/master/server/controllers/searchCtrl.js" target="_blank" rel="noopener noreferrer">Example</a>
-      </div>
-      <div>
-        <i className="devicon-sequelize-plain colored" />
-        <a href="https://github.com/Kiwilicious/CARe/blob/master/db/index.js" target="_blank" rel="noopener noreferrer">Example</a>
-      </div>
-    </div>
-    <h4 className="tech-icon-header">Other</h4>
-    <hr />
-    <div className="tech-icon">
-      <div>
-        <i className="devicon-git-plain colored" />
-      </div>
-      <div>
-        <i className="devicon-heroku-line-wordmark colored" />
-      </div>
-    </div>
-  </div>
-);
+const styles = () => ({
+  color: 'white',
+});
 
-export default IndexPage;
+const HomePage = (props) => {
+  const { classes } = props;
+  return (
+    <div>
+      <Grid container spacing={16}>
+        <Grid item sm={12}>
+          <Typography variant="headline">
+            I am a Software engineer specializing in frontend web development with a passion
+            of making elegant and lightweight single page applications. Also a game
+            enthusiast with an interest in the rising prominence of esports.
+          </Typography>
+        </Grid>
+      </Grid>
+    </div>
+  );
+};
+
+
+HomePage.propTypes = {
+  classes: PropTypes.func.isRequired,
+};
+
+export default withStyles(styles)(HomePage);
