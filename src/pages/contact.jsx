765b100d6423095c276ee8101afe0fb827b9f771
James <jhwang137@gmail.com>
2018-02-13 15:20:22 -0800

    Changed contact to FSC and finished component

--- src/pages/contact.jsx
+++ src/pages/contact.jsx
@@ -1,10 +1,19 @@
 import React from 'react';
+import Typography from 'material-ui/Typography';
 
-const Contact = (props) => {
-  const placeholder = null;
-  return (
-    <div>WIP</div>
-  );
-};
+const Contact = () => (
+  <div>
+    <Typography variant="display2" gutterBottom>
+      Contact
+    </Typography>
+    <Typography variant="subheading">
+      If you want to get a hold of me, the fastest way is through my email
+      at <a href="mailto:jhwang137@gmail.com">jhwang137@gmail.com</a>.
+      You can also find me
+      on <a href="https://www.linkedin.com/in/jameshwangcali/" target="_blank" rel="noopener noreferrer">LinkedIn</a> although
+      I don&apos;t go on everyday.
+    </Typography>
+  </div>
+);
 
 export default Contact;
