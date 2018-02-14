c272c9770241a83a11d35a4412c938e818671475
James <jhwang137@gmail.com>
2018-02-13 16:12:01 -0800

    Redid portfolio subpages with material-ui

--- src/pages/portfolio/simpleForm.jsx
+++ src/pages/portfolio/simpleForm.jsx
@@ -1,30 +1,36 @@
 import React from 'react';
+import Typography from 'material-ui/Typography';
 
-const FSFormChallenge = () => (
+const SimpleForm = () => (
   <div>
-    <div>
-      <h2>Input Form</h2>
-      <p>
+    <Typography variant="display1" gutterBottom>
+      Front end input form
+      <Typography variant="subheading">
         This challenge involved replicating the look and functionality of an input form provided in
         a gif. While most of the code was simple enough, I didn&apos;t have much experience
         implementing animation on buttons. Instead I opted to use CSS transitions to color in the
         border of the button. This challenge was done in React.
-      </p>
-      <a href="https://codepen.io/Kiwilicious/pen/RxqWXY?editors=0110" target="_blank" rel="noopener noreferrer">Code here</a>
-      <hr />
-      <h2>Ruby Implementation of Erlang Code</h2>
-      <p>
+      </Typography>
+      <Typography>
+        <a href="https://codepen.io/Kiwilicious/pen/RxqWXY?editors=0110" target="_blank" rel="noopener noreferrer">Code here</a>
+      </Typography>
+    </Typography>
+    <Typography variant="display1" gutterBottom>
+      Back end Ruby implementation of Erlang
+      <Typography variant="subheading">
         This challenge involved taking the code provided in the language Erlang and recreate it in
         Ruby. I had no experience in either language so I skimmed through the Erlang docs. I also
         took some time to learn the basics of Ruby as I had planned on taking up the language it in
         the future. Turns out the logic of the code was simple enough, although I tripped over a few
         times because of syntax. This challenge was done in vanilla Ruby.
-      </p>
-      <a href="https://gist.github.com/dudo/93c0d1ee5b627c2a6cde30616540fb7f" target="_blank" rel="noopener noreferrer">Code in Erlang</a>
-      <br />
-      <a href="https://gist.github.com/Kiwilicious/3b6728f4276fe7beb14795f830abbebe" target="_blank" rel="noopener noreferrer">Code in Ruby</a>
-    </div>
+      </Typography>
+      <Typography>
+        <a href="https://gist.github.com/dudo/93c0d1ee5b627c2a6cde30616540fb7f" target="_blank" rel="noopener noreferrer">Code in Erlang</a>
+        <br />
+        <a href="https://gist.github.com/Kiwilicious/3b6728f4276fe7beb14795f830abbebe" target="_blank" rel="noopener noreferrer">Code in Ruby</a>
+      </Typography>
+    </Typography>
   </div>
 );
 
-export default FSFormChallenge;
+export default SimpleForm;
--- src/pages/portfolio/tinyChat.jsx
+++ src/pages/portfolio/tinyChat.jsx
@@ -1,18 +1,21 @@
 import React from 'react';
+import Typography from 'material-ui/Typography';
 
-const TinyChatChallenge = () => (
+const TinyChat = () => (
   <div>
-    <div>
-      <h2>Chat Application</h2>
-      <p>
+    <Typography variant="display1" gutterButton>
+      Front end chat app
+      <Typography variant="subheading">
         This challenge involved taking the small boilerplate template provided and designing the
         client side of a simple chat app. There was a dummy python server provided but due to
         issues, I substituted it for a node/express server instead. This challenge was done in
         vanilla HTML with jQuery.
-      </p>
-      <a href="https://github.com/Kiwilicious/tinychat" target="_blank" rel="noopener noreferrer">Code here</a>
-    </div>
+      </Typography>
+      <Typography>
+        <a href="https://github.com/Kiwilicious/tinychat" target="_blank" rel="noopener noreferrer">Code here</a>
+      </Typography>
+    </Typography>
   </div>
 );
 
-export default TinyChatChallenge;
+export default TinyChat;
