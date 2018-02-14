e9e1350a8acfab34c0d7715b962f05a7fdbc6a8b
James <jhwang137@gmail.com>
2018-02-13 16:41:42 -0800

    Resolved nesting warning

--- src/pages/portfolio/tinyChat.jsx
+++ src/pages/portfolio/tinyChat.jsx
@@ -3,17 +3,17 @@ import Typography from 'material-ui/Typography';
 
 const TinyChat = () => (
   <div>
-    <Typography variant="display1" gutterButton>
+    <Typography variant="display1" gutterBottom>
       Front end chat app
-      <Typography variant="subheading">
-        This challenge involved taking the small boilerplate template provided and designing the
-        client side of a simple chat app. There was a dummy python server provided but due to
-        issues, I substituted it for a node/express server instead. This challenge was done in
-        vanilla HTML with jQuery.
-      </Typography>
-      <Typography variant="body2">
-        <a href="https://github.com/Kiwilicious/tinychat" target="_blank" rel="noopener noreferrer">Code here</a>
-      </Typography>
+    </Typography>
+    <Typography variant="subheading">
+      This challenge involved taking the small boilerplate template provided and designing the
+      client side of a simple chat app. There was a dummy python server provided but due to
+      issues, I substituted it for a node/express server instead. This challenge was done in
+      vanilla HTML with jQuery.
+    </Typography>
+    <Typography variant="body2">
+      <a href="https://github.com/Kiwilicious/tinychat" target="_blank" rel="noopener noreferrer">Code here</a>
     </Typography>
   </div>
 );
